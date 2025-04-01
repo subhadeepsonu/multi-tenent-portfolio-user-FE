5ff04c6d3682e435774132322a322c4da349f4bc
SONU <122858100+subhadeepsonu@users.noreply.github.com>
2025-04-01 19:02:41 +0530

    fff

--- src/components/dashBoard/DashBoard.tsx
+++ src/components/dashBoard/DashBoard.tsx
@@ -40,6 +40,7 @@ export default function DashboardLayout() {
 
     const handleLogout = () => {
         localStorage.removeItem("token");
+        localStorage.removeItem("show");
         navigate("/login");
     };
 
